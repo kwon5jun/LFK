f10202cc894dbb88eaf2c6dbad5172ecc5875dd5
kwon5jun <36893613+kwon5jun@users.noreply.github.com>
2025-01-03 18:43:42 +0900

    Add files via upload

---
+++ run.py
@@ -0,0 +1,78 @@
+import subprocess
+import smtplib
+import os
+import datetime
+import tomli
+from email.message import EmailMessage
+
+def buy(UID,UCNT):
+    rt_out = ''
+    buycnt=' \'\''
+    #구매 명령어 dhapi buy-lotto645 -y -p [사용자명(UID)] ''
+    cmd = 'dhapi buy-lotto645'+' -y -p '+ UID + str(buycnt)*int(UCNT)
+    print(cmd)
+    process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, encoding='utf-8')
+    while True:
+        output = process.stdout.readline()
+        if output == '' and process.poll() is not None:
+            break
+        if output:
+            rt_out = rt_out + '\n' + output.strip()
+    rt_out = rt_out + '\n'
+    
+    #예치금조회 dhapi show-balance -p [사용자명(UID)]
+    cmd = 'dhapi show-balance'+' -p '+ UID
+    print(cmd)
+    process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, encoding='utf-8')
+    while True:
+        output = process.stdout.readline()
+        if output == '' and process.poll() is not None:
+            break
+        if output:
+            rt_out = rt_out + '\n' + output.strip()
+
+    return rt_out
+
+def sand_mail(EMAIL_ADDR,EMAIL_PASSWORD,To_email,Sand_content):
+    SMTP_SERVER = 'smtp.gmail.com'
+    SMTP_PORT = 587
+
+    smtp = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.login(EMAIL_ADDR, EMAIL_PASSWORD)
+
+    dt_now = datetime.datetime.now()
+    message = EmailMessage()
+    message.set_content(Sand_content)
+    message["Subject"] = "[로또6/45 구매결과]" + str(dt_now.date())
+    message["From"] = EMAIL_ADDR
+    message["To"] = To_email
+
+    smtp.send_message(message)
+    smtp.quit()
+
+
+if __name__ == "__main__":
+    with open(os.path.expanduser("~/.dhapi/credentials"), "rb") as f:
+        data = tomli.load(f)
+
+    profile_data=data.get("Setting")
+    FW_Email = profile_data.get("ForwardingEmail")
+    FW_Passwd = profile_data.get("ForwardingPassword")
+    
+    for profile_name in data.keys():
+        if profile_name == "Setting":
+            continue
+        profile_data = data.get(profile_name)
+        print(profile_data.get("name")+" 사용자")
+        ID = profile_data.get("username")
+        Email = profile_data.get("email")
+        CNT = profile_data.get("buystat")
+        if int(CNT) > 0:
+            rt_out = buy(ID, str(CNT))
+            print('결과값', rt_out)
+            sand_mail(FW_Email,FW_Passwd,Email, rt_out)
+        else:
+            print("미구매")
+
